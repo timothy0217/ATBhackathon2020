57d6d13acf35b00a5a68bdaa7c5a17c31e34cb59
timothy tsai <timothy@routeique.com>
2020-02-08 13:29:44 -0700

    no message

--- laravel/app/Http/Service/ATBAPI.php
+++ laravel/app/Http/Service/ATBAPI.php
@@ -9,7 +9,10 @@ use Illuminate\Support\Facades\DB;
 class ATBAPI
 {
     private  $_baseAPI = "https://api.leapos.ca"; //Helcim Commerce API - URL Location
-    private  $_bankID = "cb4f94251b14ec41ce8444022a61e26"; //Helcim Commerce API - URL Location
+    private  $_userName = "bb3c98c6a7e3f34a6a9b140"; //Helcim Commerce API - URL Location
+    private  $_password = "dc08d4b3f3f4b3K+"; //Helcim Commerce API - URL Location
+    private  $_consumer_key = "89c7a0d1727b47a1ac18b65fd4a96ae6"; //Helcim Commerce API - URL Location
+    private  $_bankID = "bb3c98c6a7e3f34a6a9b1405e8a8d74"; //Helcim Commerce API - URL Location
 
     public function login(){ // process credit card using new card
 
@@ -19,7 +22,7 @@ class ATBAPI
         // API call using guzzle
         try
         {
-            $client = new Client(['headers' => ['Authorization' =>'DirectLogin username="bb3c98c6a7e3f34a6a9b140", password="dc08d4b3f3f4b3K+", consumer_key="89c7a0d1727b47a1ac18b65fd4a96ae6"']]);
+            $client = new Client(['headers' => ['Authorization' =>'DirectLogin username="'.$this->_userName.'", password="'.$this->_password.'", consumer_key="'. $this->_consumer_key .'"']]);
 
             // use post to send data to Helcim api end point.
             $result = $client->request('POST',$apiFullAddress)->getBody()->getContents();
@@ -31,7 +34,6 @@ class ATBAPI
         }
         // return xml
         $json = json_decode($result,TRUE);
-        dd($json);
 
         // convert to json
         // decode json to array
@@ -39,8 +41,7 @@ class ATBAPI
     }
 
     public function getBank($token){ // process credit card using new card
-
-        $apiDirectory = "/obp/v4.0.0/banks/cb4f94251b14ec41ce8444022a61e26";
+        $apiDirectory = "/obp/v4.0.0/banks/".$this->_bankID;
         $apiFullAddress = $this->_baseAPI . $apiDirectory;
 //
 //        $body = array(
@@ -104,5 +105,8 @@ dd($json);
         // decode json to array
         return $json;
     }
+
+
+
 
 }
