0ee0fa6430be3c60c0314107a5009448ee9c6fab
Joshua <joshua@routeique.com>
2020-02-08 17:23:04 -0700

    Added forms

---
+++ laravel/resources/js/components/Contributions/Contributions.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+import {Container, Typography, TextField, Button} from '@material-ui/core';
+import '../../../sass/RegistrationPage.scss';
+
+export default class Passions extends Component {
+    constructor(props) {
+        super(props);
+
+        // Set the initial state for the form elements to empty values
+        this.state = {
+            'contribution_one': '',
+            'contribution_two': '',
+            'contribution_three': '',
+        };
+    }
+
+    updateFormState(event) {
+        const obj  = {};
+        obj[event.target.id] = event.target.value;
+        this.setState(obj);
+    }
+
+    handleFormSubmit() {
+        console.log('handle form submit', this.state);
+        localStorage.setItem('contribution_one', this.state.contribution_one);
+        localStorage.setItem('contribution_two', this.state.contribution_two);
+        localStorage.setItem('contribution_three', this.state.contribution_three);
+
+        console.log('local storage', Object.entries(localStorage));
+
+        // Redirect to next page
+        // TODO: Redirect to loading page
+        this.props.history.push('/resultsPage');
+    }
+
+    render() {
+        return (
+            <Container>
+                <div className="form-container">
+                    <Typography variant="h4">
+                        Set Your Contributions
+                    </Typography>
+                    <br /><br />
+
+                    <form noValidate autoComplete="off">
+                        <TextField id="contribution_one" label="Contribution 1" variant="filled" fullWidth
+                                   onChange={this.updateFormState.bind(this)}/>
+                        <br />
+                        <TextField id="contribution_two" label="Contribution 2" variant="filled" fullWidth
+                                   onChange={this.updateFormState.bind(this)}/>
+                        <br />
+                        <TextField id="contribution_three" label="Contribution 3" variant="filled" fullWidth
+                                   onChange={this.updateFormState.bind(this)}/>
+                        <br />
+
+                        <center>
+                            <Button className="btn btn-primary submit"
+                                    disableElevation
+                                    onClick={() => this.handleFormSubmit()}
+                            >
+                                Submit
+                            </Button>
+                        </center>
+                    </form>
+                </div>
+            </Container>
+        );
+    }
+}
--- laravel/resources/js/components/Index.js
+++ laravel/resources/js/components/Index.js
@@ -4,10 +4,11 @@ import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Home from './Home/Home';
 import Main from './Home/Main';
 import RegistrationPage from './RegistrationPage/RegistrationPage';
-import PassionPage from './Passions/passions';
+import PassionPage from './Passions/Passions';
 import ResultsPage from './Results/results';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../sass/app.scss';
+import Contributions from "./Contributions/Contributions";
 
 class Index extends Component {
     render() {
@@ -15,10 +16,11 @@ class Index extends Component {
             <HashRouter>
                 <Switch>
                     <Route exact path='/' component={Home} />
-                    <Route exact path='/setup' component={Main} />
+                    <Route exact path='/main' component={Main} />
                     <Route path='/registerPage/' component={RegistrationPage} />
                     <Route path='/passionPage/' component={PassionPage} />
                     <Route path='/resultsPage/' component={ResultsPage} />
+                    <Route path='/contributionsPage/' component={Contributions} />
                     <Redirect to='/' />
                 </Switch>
             </HashRouter>
