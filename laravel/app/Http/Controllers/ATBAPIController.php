24f32956f3499d2089ddab24a7029783b8736480
timothy tsai <timothy@routeique.com>
2020-02-08 21:55:03 -0700

    no message

--- laravel/app/Http/Controllers/ATBAPIController.php
+++ laravel/app/Http/Controllers/ATBAPIController.php
@@ -8,6 +8,8 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ConfirmsPasswords;
 use Illuminate\Http\Request;
 use App\Http\Service\ATBAPI;
+use Illuminate\Support\Facades\Storage;
+
 
 
 
@@ -22,23 +24,68 @@ class ATBAPIController extends Controller
         $token = $login['token'];
         session()->put('key',$token);
         $token = session()->get('key');
+// fetch all accounts and it's ID
+        $getAllAccounts = $ATBAPI->getAccount($token);
+        $getAccountID =$getAllAccounts[1]['id']; // Change the number to choose between accounts
+        $getTransactions = $ATBAPI->getTransactionsForAccount($token,$getAccountID);
+        $getTransactions = json_encode($getTransactions,true);
+        $accountID= $getAccountID;
+        $fileName = $accountID.'.json';
 
-     //$getAllAccounts = $ATBAPI->getAccount($token);
-     //dd($getAllAccounts);
-        $test=$ATBAPI->getTransactionsForAccount($token,'2524119902112-a6b71584-74f');
+//  Save Json as file to storage
+        Storage::put('public/upload/' . $fileName, $getTransactions);
+//  Read json file from storage
+        $path = storage_path().'/app/public/upload/'.$fileName;
+        $oldJason = json_decode(file_get_contents($path),true);
 
+// Nordigen Formating
+        $accountListArray = array('account_nr'=>$accountID,
+            'holder_name'=>'first_name last_name',
+            'holder_id'=>$accountID,
+            'bank_name'=>'screaming lemon',
+            'currency'=>'CAD',
+            'start_balance'=>500000,
+            'end_balance'=>400000,
+            'debit_turnover'=>101000,
+            'credit_turnover'=>1000,
+            'period_start'=>'2020-01-01',
+            'period_end'=>'2020-01-01',
+            'transaction_list'=>[]
+        );
 
+        // Transaction from ATB Finance
+        $transactions = $oldJason['transactions'];
+        // Unique transaction ID
+        $transactionID = 0;
 
-        $types = [];
+        foreach($transactions as $transaction)
+        {
+            // Unique transaction ID
+            $transactionID = $transactionID+1;
+            $date = $transaction['details']['posted'];
 
-        foreach($test['transactions'] as $t){
+            $transactionListArray = array(
+                'date'=>substr($date,0,10),
+                'partner'=>$transaction['details']['type'],
+                'info'=>$transaction['details']['description'] . ' '. str_replace($date,'T','') . ' CAD',
+                'transaction_id'=>'arbritary-unique-id'.$transactionID,
+                'sum' => (float)$transaction['details']['value']['amount']
+            );
 
-            $type = $t['details']['type'];
-            array_push($types, $type);
+            // push transaction_list data as an object under  transaction_list array
+            $transactionListObject = (object)$transactionListArray;
+            array_push($accountListArray['transaction_list'],$transactionListObject);
         }
 
-        return $types;
 
+
+        // push account_list data as an object under account_list array
+        $accountListObject = (object)$accountListArray;
+        $newJson = array('account_list'=>[]);
+        array_push($newJson['account_list'],$accountListObject);
+        $newJson = json_encode($newJson);
+//Nordigen json format --->  $newJason
+        dd($newJson);
 
         return view('ATBAPI');
     }
